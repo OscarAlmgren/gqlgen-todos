787b7604c1af11bce7d4af1cb6a8acfbc005c08d
Oscar Almgren <oscar.almgren@gmail.com>
2022-09-15 15:41:53 +0200

    Move env loading to server.go

--- internal/pkg/db/migrations/mongodb/mongodb.go
+++ internal/pkg/db/migrations/mongodb/mongodb.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -15,10 +14,6 @@ import (
 var MongoDbClient *mongo.Client
 
 func InitMongoDB() {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
-	}
-
 	uri := os.Getenv("MONGODB_URI")
 	if uri == "" {
 		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
