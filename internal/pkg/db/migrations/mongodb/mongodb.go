de680c0a7cfb873a92c468259c69764a2ca7de61
Oscar Almgren <oscar.almgren@gmail.com>
2022-09-15 11:26:03 +0200

    Initial mongo add

--- internal/pkg/db/migrations/mongodb/mongodb.go
+++ internal/pkg/db/migrations/mongodb/mongodb.go
@@ -1,11 +1,36 @@
 package database
 
-import "github.com/golang-migrate/migrate/v4/database/mongodb"
+import (
+	"context"
+	"log"
+	"os"
 
-var MongoDB *mongodb.Config
+	"github.com/golang-migrate/migrate/v4/database/mongodb"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
-func InitMongoDB() {
+var MongoDbConfig *mongodb.Config
 
+var MongoDbClient *mongo.Client
+
+func InitMongoDB() *mongo.Client {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+
+	MongoDbClient, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	return MongoDbClient
 }
 
 func MigrateMongoDB() {
@@ -13,5 +38,7 @@ func MigrateMongoDB() {
 }
 
 func CloseMongoDB() {
-
+	if err := MongoDbClient.Disconnect(context.TODO()); err != nil {
+		panic(err)
+	}
 }
